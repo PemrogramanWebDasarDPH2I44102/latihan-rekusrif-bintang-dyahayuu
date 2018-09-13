53684729035c817fa73baf2e28837aeafd8b744e
dyahayuu <42660778+dyahayuu@users.noreply.github.com>
2018-09-13 08:55:00 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,40 @@
-<?php
+<?php 
+	$nilai = 5;
+	function satu($y, $u){
+		if ($y > 0) {
+			if ($u > 0) {
+				echo "* ";
+				$u--;
+				satu($y, $u);
+			}
+			else{
+				echo "<br>";
+				$y--;
+				satu($y, $y);
+			}
+			
+		}
+	}
+	satu($nilai, $nilai);
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
+	echo "=======================<br><br>";
+	$nilai= 5;
+	function dua($y, $u, $nilai){
+		if ($y <= $nilai) {
+			if ($u <= $y) {
+				echo "$u";
+				$u++;
+				dua($y, $u, $nilai);
+			}
+			else{
+				echo "<br>";
+				$y++;
+				dua($y, 1 ,$nilai);
+			}
+			
+		}
+	}
+	dua(1,1,$nilai);
+
+
 ?>
